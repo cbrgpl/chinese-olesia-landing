4f993b53066e7dd5ad7ef3ef3f69306ea1476b11
cbrgpl <cybirgpl@gmail.com>
2024-12-10 19:35:44 +0700

    fix(feedbacks): fix cached avito feedbacks;

--- src/static/avitoCachedFeedbacks.ts
+++ src/static/avitoCachedFeedbacks.ts
@@ -2,10 +2,23 @@ import { type IFeedback, EFeedbackOrigins } from '@/static/feedbackUtils';
 
 export const feedbacks: IFeedback[] = [
   {
-    avatar: null,
-    name: 'Раболина',
-    date: '2024-07-08T21:42',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias neque nihil itaque delectus minus adipisci qui doloremque accusamus. At fugiat libero hic nulla esse assumenda laborum quibusdam qui, voluptates repudiandae reprehenderit nam veniam exercitationem ipsam?',
+    avatar: 'https://70.img.avito.st/image/1/1.Q8jbFba1-SHtsm0n0QRJ2vK07SFlvm0n7bLtIw.gOKMY79PXOB2DRTQmEkNePbBJRy3XCh8O31MRcr6g_k',
+    name: 'Катя',
+    date: '2022-07-24T00:00',
+    text: 'Первый урок прошёл на высшем уровне. Где были неясности Олеся сразу же остановилась и помогла разъяснить все. Так же отдельно хочется сказать про формат урока - приятная глазу презентация, ничего лишнего и мешающего усвоению материала. Было крайне интересно смотреть. Очень креативный подход !',
+    origin: EFeedbackOrigins.AVITO,
+    extras: {
+      vkExtras: null,
+      avitoExtras: {
+        score: 5,
+      },
+    },
+  },
+  {
+    avatar: 'https://70.img.avito.st/image/1/1.VBXm97a27vzQUHr6wvJPYg5U-PZY1P5-VFT6.IRdaT9VaWdg-pZbE-AfkGi6Pq8HaDR7ajWpTawtW4Fo',
+    name: 'Евгения',
+    date: '2022-06-11T00:00',
+    text: 'Отличный репетитор! Материал преподносится очень доступно и легко!',
     origin: EFeedbackOrigins.AVITO,
     extras: {
       vkExtras: null,
@@ -16,27 +29,14 @@ export const feedbacks: IFeedback[] = [
   },
   {
     avatar: null,
-    name: 'Lorem, ipsum dolor ',
-    date: '2024-07-09T14:17',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quisquam, enim veritatis tempore minima eos cum delectus veniam molestias! Voluptatum saepe magnam, sapiente adipisci aspernatur nesciunt esse non atque, maxime, hic dignissimos fugit assumenda odit blanditiis ea? Consequatur nam laudantium rem impedit earum soluta ipsam molestiae placeat fuga ducimus delectus alias expedita ex nemo sed, ullam doloribus eveniet labore aliquam iusto magnam eaque quod quisquam officia. Praesentium numquam, officiis itaque doloribus natus debitis! Veritatis beatae esse ut non, recusandae architecto officiis! Perspiciatis, dicta quo at nihil iure provident, porro magni neque fuga quae officiis ab aliquam minima, officia eligendi rerum. Consequuntur corporis asperiores reiciendis, ipsum deserunt odit atque dolorum blanditiis ducimus facere veniam officiis, voluptas ipsam quasi repellendus dolores repudiandae eum ea ullam tempore natus dolor? Ipsam officiis error expedita est debitis rem. Nemo eum est natus sunt vel! Quisquam mollitia nobis placeat in libero expedita voluptatum laborum, itaque qui cumque non temporibus omnis molestiae tenetur necessitatibus voluptas earum rem est odio enim ab eius. Quia corrupti sequi aliquid temporibus sint recusandae, consequatur a sunt possimus saepe pariatur voluptatum fugiat vero sapiente quibusdam officia tenetur? Sunt aut nemo voluptatibus accusantium qui corrupti perspiciatis inventore similique, cupiditate deserunt nulla quasi placeat voluptates, illo maxime hic vel ullam modi cum. Atque officia expedita nisi iusto voluptatem molestiae dicta quo accusamus perspiciatis illum. Veritatis quas nihil sequi deleniti aspernatur reprehenderit ducimus a accusantium. Nam veritatis ipsum beatae totam, quis quod doloribus odit esse delectus a quos alias unde nulla ad fugiat natus. Nisi.',
+    name: 'Майя',
+    date: '2022-04-18T00:00',
+    text: 'Олеся быстро ответила а запрос, провела первое ознакомительное занятие. Нам все понравилось.',
     origin: EFeedbackOrigins.AVITO,
     extras: {
       vkExtras: null,
       avitoExtras: {
-        score: 4,
-      },
-    },
-  },
-  {
-    avatar: null,
-    name: 'Lorem, ipsum',
-    date: '2024-07-09T14:17',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo alias sint adipisci quod tempore enim perferendis sapiente totam exercitationem natus dicta harum ea blanditiis consectetur repellat, fugit cum nostrum ad unde excepturi asperiores eos aliquid porro suscipit. Harum quae ipsa, aspernatur quidem officia beatae tempore cum! Sapiente inventore beatae laudantium.',
-    origin: EFeedbackOrigins.AVITO,
-    extras: {
-      vkExtras: null,
-      avitoExtras: {
-        score: 3,
+        score: 5,
       },
     },
   },
