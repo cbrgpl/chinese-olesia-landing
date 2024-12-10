c9379025ee1e73d79341de44932a6eb418515219
cbrgpl <cybirgpl@gmail.com>
2024-12-10 20:27:52 +0700

    fix: add requestIdleCallback;

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import { allowImagesLoading } from './components/image/image';
 
 // 3. Придумать как еще можно грузить картинки кроме load-ивента
 // 4. Сделать, чтобы webp картинки тоже подгружались
+if (!window.requestIdleCallback) {
+  // TODO Add normal polyfill
+  window.requestIdleCallback = ((cb: (...args: any) => any) => {
+    setTimeout(() => {
+      cb();
+    }, 200);
+  }) as any;
+}
 
 window.addEventListener('load', async () => {
   await import('./styles/index.scss' as any);
